6f91ab83c5d6c9cacea40a96f0e04f8ec3bd0a65
bradapratt <bradapratt@gmail.com>
2021-01-27 15:31:36 -0800

    Added GameClient, started filling out Game.java.

--- src/com/codebusters/game/Game.java
+++ src/com/codebusters/game/Game.java
@@ -3,10 +3,27 @@ package com.codebusters.game;
 import java.util.ArrayList;
 
 public class Game {
-    GameState currentGame;
-    Viewer GUI;
-    TextParser parser;
-    ArrayList<Items> inventory;
-    ArrayList<Chapter> story;
+    public GameState currentGame;
+    public Viewer GUI;
+    public TextParser parser;
+    public ArrayList<Items> inventory;
+    public ArrayList<Chapter> story;
 
+    public Game() {
+        currentGame = new GameState();
+        GUI = new Viewer();
+        parser = new TextParser();
+        inventory = new ArrayList<>();
+        story = new ArrayList<>();
+    }
+
+    public void startGame(){
+        boolean endGame = false;
+        Chapter currentChapter = story.get(0);
+
+        while (!endGame){
+            updateGameState(currentChapter);
+            updateViewer();
+        }
+    }
 }
---
+++ src/com/codebusters/game/client/GameClient.java
@@ -0,0 +1,17 @@
+package com.codebusters.game.client;
+/**
+ * Main class for the Quest for the Quarantine game.
+ *
+ * Authors: Bradley Pratt, Debbie Bitencourt, Aliona Demerau, Dustin Morris
+ * Last Edited: 01/27/2021
+ */
+
+import com.codebusters.game.Game;
+import java.io.IOException;
+
+public class GameClient {
+    public static void main(String[] args) throws IOException {
+        Game game1 = new Game();
+        game1.startGame();
+    }
+}
