21c28fe1b993929b956db2765f109d794657bbd7
bradapratt <bradapratt@gmail.com>
2021-01-28 11:56:46 -0800

    Filled out GameState.java. Completed V1 of Game.java. Still need to add DB import and creation of Chapter list.

--- src/com/codebusters/game/GameState.java
+++ src/com/codebusters/game/GameState.java
@@ -1,4 +1,36 @@
 package com.codebusters.game;
+/**
+ * GameState.java represents the current gamestate of the ongoing game.
+ * It stores the current scene text and inventory, which the GUI can then
+ * access to display the interface for the player. It is updated by Game.java.
+ * <p>
+ * Authors: Bradley Pratt & Debbie Bitencourt
+ * Last Edited: 01/28/2021
+ */
+import java.util.ArrayList;
 
 public class GameState {
+    private String sceneText;
+    private ArrayList<Items> inventory;
+
+    public GameState(){
+        setSceneText("");
+        inventory = new ArrayList<>();
+    }
+
+    public String getSceneText() {
+        return sceneText;
+    }
+
+    public void setSceneText(String sceneText) {
+        this.sceneText = sceneText;
+    }
+
+    public ArrayList<Items> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(ArrayList<Items> inventory) {
+        this.inventory = inventory;
+    }
 }
