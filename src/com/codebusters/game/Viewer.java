14e4c2866eba84a123aa506dd58177fcea12930f
kitsune777 <watermonkey777gmail.com>
2021-02-02 14:44:36 -0800

    viewer class: updated gui. text is readable and gui is more appealing.

--- src/com/codebusters/game/Viewer.java
+++ src/com/codebusters/game/Viewer.java
@@ -2,6 +2,8 @@ package com.codebusters.game;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,16 +11,18 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class Viewer implements ActionListener{
-    JFrame window;
+public class Viewer implements ActionListener {
     Container container;
-    JPanel titlePanel, storyPanel, inventoryPanel;
+    JPanel storyPanel, inventoryPanel;
     JLabel titleName, inventoryTitle;
-    JTextArea storyTextArea, inputTextArea, inventoryTextArea;
-    Font titleFont = new Font("Times New Roman", Font.BOLD, 36);
+    JTextArea storyTextArea, inventoryTextArea;
+    Font titleFont = new Font("Times New Roman", Font.BOLD, 32);
     Font normalFont = new Font("Times New Roman", Font.PLAIN, 16);
     JTextField userInputField;
     JButton inputBtn = new JButton("Enter");
+    Border dashed = BorderFactory.createDashedBorder(Color.decode("#0D5B69"), 1.4f, 8.0f, 2.0f, true);
+    Border empty = BorderFactory.createEmptyBorder(1, 1, 1, 1);
+    Border compound = new CompoundBorder(empty, dashed);
 
     String input;
     boolean waitingForInput;
@@ -27,14 +31,14 @@ public class Viewer implements ActionListener{
     public Viewer() {
         waitingForInput = true;
 
-        window = new JFrame(); //initiates the frame
-        window.setSize(800,560); //size for the frame
+        JFrame window = new JFrame(); //initiates the frame
+        window.setSize(800, 560); //size for the frame
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //closes the window
 
         //create background image
         BufferedImage img = null;
         try {
-            img = ImageIO.read(new File("pageImg.png"));
+            img = ImageIO.read(new File("bgImage.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -49,85 +53,98 @@ public class Viewer implements ActionListener{
         container = window.getContentPane(); //container inside the window that contains all the content
         container.add(background);
 
-        //panel for title
-        titlePanel = new JPanel();
-        titlePanel.setBounds(180,50, 480, 60);
-        titlePanel.setBackground(Color.decode("#F5EDDA"));
+        //panel for game title
+//        titlePanel = new JPanel();
         titleName = new JLabel("The Quest for Quarantine");
+        titleName.setBounds(250, 60, 480, 46);
+//        titleName.setBackground(Color.decode("#EDE5D0"));
         titleName.setForeground(Color.decode("#e76f51")); //title text color
         titleName.setFont(titleFont); //title font
-        titlePanel.add(titleName);
-        container.add(titlePanel);
+//        titlePanel.add(titleName);
+        container.add(titleName);
 
-        //user input text area
-        inputTextArea = new JTextArea();
-        inputTextArea.setBounds(180, 380, 150, 30);
-        inputTextArea.setBackground(Color.decode("#F5EDDA"));
-        inputTextArea.setForeground(Color.decode("#2E8B57"));
-        container.add(inputTextArea);
+        //text panel for main story
+        storyPanel = new JPanel();
+        storyPanel.setBounds(160, 115, 390, 280);
+        storyPanel.setBackground(Color.decode("#EDE5D0"));
+        //storyPanel.setBorder(BorderFactory.createLoweredBevelBorder());
+        container.add(storyPanel); //adding story panel to main container
 
         //user input
         userInputField = new JTextField();
         userInputField.setText("");
-        userInputField.setBounds(180,340,110,30);
-        userInputField.setBackground(Color.decode("#F5EDDA"));
+        userInputField.setBounds(154, 420, 110, 30);
+        userInputField.setBackground(Color.decode("#EDE5D0"));
+        userInputField.setBorder(BorderFactory.createLoweredBevelBorder());
         userInputField.setForeground(Color.black);
         userInputField.addActionListener(this);
         container.add(userInputField);
 
         //input button
-        inputBtn.setBounds(292,340,80,30);
+        inputBtn.setBounds(270, 420, 80, 30);
         inputBtn.addActionListener(this);
         inputBtn.setForeground(Color.white);
         inputBtn.setBackground(Color.darkGray);
-        inputBtn.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+        inputBtn.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+        inputBtn.setBorder(BorderFactory.createRaisedBevelBorder());
         userInputField.add(inputBtn);
         container.add(inputBtn);
 
-        //text panel for main story
-        storyPanel = new JPanel();
-        storyPanel.setBounds(180,115,480, 200);
-        storyPanel.setBackground(Color.decode("#F5EDDA"));
-        container.add(storyPanel); //adding story panel to main container
+//        //user input text area
+//        inputTextArea = new JTextArea();
+////        inputTextArea.setBounds(160, 420, 150, 30);
+////        inputTextArea.setBackground(Color.decode("#EDE5D0"));
+////        inputTextArea.setForeground(Color.decode("#2E8B57"));
+//        container.add(inputTextArea);
 
         //panel for inventory
         inventoryPanel = new JPanel();
-        inventoryPanel.setBounds(480,320, 180, 100);
-        inventoryPanel.setBackground(Color.decode("#F5EDDA"));
+        inventoryPanel.setBounds(570, 120, 120, 280);
+        inventoryPanel.setBackground(Color.decode("#EDE5D0"));
         inventoryPanel.setForeground(Color.decode("#e76f51")); //title text color
+        inventoryPanel.setBorder(compound);
         inventoryPanel.setFont(normalFont); //font
         container.add(inventoryPanel);
+
+        //inventory title
+        inventoryTitle = new JLabel("INVENTORY");
+        inventoryTitle.setForeground(Color.decode("#635147")); //title text color
+        inventoryTitle.setFont(new Font("Arial", Font.BOLD, 13)); //title font
+        inventoryPanel.add(inventoryTitle);
 
     }
 
     public void updateViewer() {
-        inventoryTitle = new JLabel("Inventory");
-        inventoryTitle.setForeground(Color.decode("#e76f51")); //title text color
-        inventoryTitle.setFont(normalFont); //title font
-        titlePanel.add(titleName);
+
+
+        container.add(titleName);
 
         String inv = "";
-        for (Items item: GameState.getInstance().getInventory()){
+        for (Items item : GameState.getInstance().getInventory()) {
             inv += item + "\n";
         }
         inventoryTextArea = new JTextArea(inv);
-        inventoryTextArea.setBounds(480,320,180, 100);
-        inventoryTextArea.setBackground(Color.decode("#F5EDDA"));
+        inventoryTextArea.setBounds(160, 420, 100, 100);
+        inventoryTextArea.setBackground(Color.decode("#EDE5D0"));
         inventoryTextArea.setForeground(Color.black);
         inventoryTextArea.setFont(normalFont);
         inventoryTextArea.setLineWrap(true);
+        inventoryTextArea.setWrapStyleWord(true);
+
         inventoryPanel.removeAll();
         inventoryPanel.add(inventoryTitle);
         inventoryPanel.add(inventoryTextArea);
         inventoryTextArea.update(inventoryTextArea.getGraphics()); //updates text area
 
         //text area of the main story
-        storyTextArea = new JTextArea(GameState.getInstance().getSceneText());
-        storyTextArea.setBounds(180,150,480, 190);
-        storyTextArea.setBackground(Color.decode("#F5EDDA"));
+        storyTextArea = new JTextArea(GameState.getInstance().getSceneText()); //connects to the story text in the game.
+        storyTextArea.setBounds(120, 120, 360, 280);
+        storyTextArea.setBackground(Color.decode("#EDE5D0"));
         storyTextArea.setForeground(Color.black);
         storyTextArea.setFont(normalFont);
-        storyTextArea.setLineWrap(true);
+        storyTextArea.setLineWrap(true); //wraps text to keep it inside panel.
+        storyTextArea.setWrapStyleWord(true);//creates readable text that is separated by word when wrapped.
+
         storyPanel.removeAll();
         storyPanel.add(storyTextArea);
         storyTextArea.update(storyTextArea.getGraphics()); //updates text area
@@ -136,10 +153,10 @@ public class Viewer implements ActionListener{
     }
 
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==userInputField || e.getSource() == inputBtn) {
+        if (e.getSource() == userInputField || e.getSource() == inputBtn) {
 
             input = userInputField.getText();
-            inputTextArea.setText(input);
+            //inputTextArea.setText(input); //uncomment to use as feedback to show user's text entered
             TextParser.getInstance().parseInput(input);
 
             userInputField.setText("");
